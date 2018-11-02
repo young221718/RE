0d21f0d7538b0209985419b46e7fb049ecd84387
Young2218 <44011517+Young2218@users.noreply.github.com>
2018-11-02 19:47:25 +0900

    채팅방 연결이 끝났습니다.
    채팅방 연결이 되기 시작했다.
    
    채팅 프로토콜을 수정을 해야하지만 ㅠㅠ.....................

--- Network_Team_Project/src/Client.java
+++ Network_Team_Project/src/Client.java
@@ -1,7 +1,9 @@
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 import basic.RoomInformation;
 
@@ -16,9 +18,8 @@ public class Client {
 			ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
 			ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
 
-			// 채팅방만들기 =======================================================
-			Integer pro = new Integer(111); // 채팅방을 만들기 위한 프로토콜 111
-			out.writeObject(pro);
+			// 채팅방 만들기 =======================================================
+			out.writeInt(111);// 채팅방을 만들기 위한 프로토콜 111
 			out.flush();
 			// 방 정보 보내기
 			RoomInformation roomInfor = new RoomInformation("group1", -1, 3, "Who are you?", "I don't know", null);
@@ -28,36 +29,91 @@ public class Client {
 			int roomNum = (Integer) in.readObject();
 			System.out.println("Room: " + roomNum);
 			// ================================================================
-			
-			
-			// 채팅방들어가기
-			pro = new Integer(222);
-			out.writeObject(pro);
+
+			// 채팅방 들어가기 =======================================================
+			out.writeInt(222);// 채팅방에 들어가기 위한 프로토콜 222
 			out.flush();
+			// 입력한 핀번호 혹은 서버에게로 받은 핀번호를 서버한테 보낸다.
 			out.writeObject(roomNum);
-
-			// 채팅방의 소켓, in, out 연결하기
-			Socket chatSocket = new Socket(host, roomNum);
-			ObjectOutputStream cout = new ObjectOutputStream(chatSocket.getOutputStream());
-			ObjectInputStream cin = new ObjectInputStream(chatSocket.getInputStream());
-
-			// 채팅방 test
-			for (int i = 0; i < 10; i++) {
-				cout.writeObject((Integer) i);
-				cout.flush();
-				System.out.println((String) cin.readObject());
+			out.flush();
+			// =================================================================
+			
+			// 채팅을 시작함========================================================
+			Chatting ch = new Chatting(roomNum);
+			ch.start(); // 채팅 쓰레드를 시작
+			while(ch.isAlive()) {
+				// 채팅방이 살아있을 때까지 아무것도 안함
+				// TODO: 수정이 필요함
 			}
-
+			//==================================================================
+			System.out.println("Chatting is dead");
+			
+			// 종료하기 ==========================================================
+			out.writeInt(888); // protocol bye-bye-bye
+			// 스트림과 소켓 닫기
 			out.close();
 			in.close();
-			chatSocket.close();
 			clientSocket.close();
-
+			// ================================================================
+		
 		} catch (Exception e) {
 			System.out.println("Error");
 		}
-
 		System.out.println("Client End");
 	}
+	
+	// 채팅방 쓰레드 =====================================================================
+	private static class Chatting extends Thread {
+		Socket chatSocket;
+		ObjectInputStream cin;
+		ObjectOutputStream cout;
 
+		/**
+		 * Chatting Constructor
+		 * @param rm : room number의 줄임말
+		 */
+		public Chatting(int rm) {
+			try {
+				InetAddress host = InetAddress.getLocalHost(); // 로컬 주소 받아오기
+				this.chatSocket = new Socket(host, rm);
+				this.cout = new ObjectOutputStream(chatSocket.getOutputStream());
+				this.cin = new ObjectInputStream(chatSocket.getInputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void run() {
+			// 채팅방 test 
+			Scanner keyboard = new Scanner(System.in);
+			try {
+				
+				for (int i = 0; i < 1; i++) {
+					System.out.print("Enter Message: ");
+					// 이 부부은 바꿔서 키보드로 입력하는 부분에 넣으면 될것같아요! =============
+					String temp = keyboard.nextLine();
+					cout.writeObject(temp);
+					cout.flush();
+					// =====================================================
+					System.out.println((String) cin.readObject()); // 이건 채팅 뜨는데에 넣으면 될것같아요
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					// 소켓과 스트림 종료
+					chatSocket.close();
+					cin.close();
+					cout.close();
+					keyboard.close();
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	// ===============================================================================
 }
--- Network_Team_Project/src/WaitingRoom.java
+++ Network_Team_Project/src/WaitingRoom.java
@@ -10,12 +10,11 @@ import basic.Room;
 import basic.RoomInformation;
 
 public class WaitingRoom extends Room {
-	
+
 	public static HashMap<Integer, ServerSocket> chatRoomServerSockets = new HashMap<Integer, ServerSocket>();
 	public static HashMap<Integer, ServerSocket> fileRoomServerSockets = new HashMap<Integer, ServerSocket>();
-	
+
 	private static Integer ROOMPIN = new Integer(10000);
-	private static final int PROTOCOL_NUMBER = 10;
 
 	WaitingRoom(Socket socket) {
 		super(socket);
@@ -29,17 +28,14 @@ public class WaitingRoom extends Room {
 				break; // 쓰레기 코드
 			}
 
-			
 			toClient = new ObjectOutputStream(roomSocket.getOutputStream());
 			fromClient = new ObjectInputStream(roomSocket.getInputStream());
 
-			
 			// 사용자가 waiting room에서 하는 일을 확인
 			// 소켓이 연결되어 있을 때까지 유지된다.
 			while (roomSocket.isConnected()) {
-				
-				while(fromClient.available() > 0);
-				protocol = (Integer) fromClient.readObject();
+
+				protocol = (Integer) fromClient.readInt();
 				System.out.println("protocol: " + protocol);
 				// protocol
 				// 111 : 방을 만들고 싶다.
@@ -49,13 +45,12 @@ public class WaitingRoom extends Room {
 					// TODO : 서버에서 체크할건지, 클라이언트에서 체크할건지 생각해 보자
 					roomInfor = (RoomInformation) fromClient.readObject();
 					roomInfor.print();
-					
-					
+
 					// 방 만들기 --> 서버 소켓을 만들어 놓는다.
 					// 방 만들기를 요청한 클라이언트에게 핀번호를 전송해준다.
 					try {
 						System.out.println("Enter protocol 111");
-						int roomNumber = makeChatRoom(roomInfor);
+						int roomNumber = makeChatRoom();
 						toClient.writeObject(roomNumber);
 						toClient.flush();
 						System.out.println(roomNumber + " room made");
@@ -68,32 +63,37 @@ public class WaitingRoom extends Room {
 
 				} else if (222 == protocol) { // Enter the room
 					System.out.println("Enter protocol 222");
-					int PIN = (Integer)fromClient.readObject();
+					int PIN = (Integer) fromClient.readObject();
 					System.out.println("Enter room Pin in " + PIN);
 					enterChatRoom(PIN);
 
+				} else if (888 == protocol) {
+					toClient.close();
+					fromClient.close();
+					break;
 				} else {
 					// TODO : 프로토콜 예외 처리해야함.
 				}
 			}
 
 		} catch (ClassNotFoundException e) {
+			System.out.println("Error 1");
 			e.printStackTrace();
 		} catch (IOException e) {
+			System.out.println("Error 2");
 			e.printStackTrace();
 		} catch (Exception e) {
+			System.out.println("Error 3");
 			e.printStackTrace();
 		} finally {
-			/*
-			 * 사용자 로그 아웃 시켜야함
-			 */
 			try {
-				roomSocket.close();
+				if (!roomSocket.isClosed())
+					roomSocket.close();
 			} catch (IOException e) {
 
 			}
 		}
-		
+
 		System.out.println("End Watiing Room");
 	}
 
@@ -111,6 +111,8 @@ public class WaitingRoom extends Room {
 
 	}
 
+	// For ChatRoom
+	// ============================================================================================
 	/**
 	 * makeRoom
 	 * 
@@ -125,11 +127,11 @@ public class WaitingRoom extends Room {
 		synchronized (chatRoomServerSockets) {
 			PIN = makePIN();
 			if (chatRoomServerSockets.containsKey((Integer) PIN)) {
-				System.out.println("Already Exist");
+				System.out.println("Already Exist - ChatRoom:" + PIN);
 			}
 			ServerSocket tempSS = new ServerSocket(PIN);
-			rf.port = PIN;
-			chatRoomServerSockets.put(rf, tempSS);
+			roomInfor.port = PIN;
+			chatRoomServerSockets.put(PIN, tempSS);
 		}
 		System.out.println("end makeChatRoom");
 		return PIN;
@@ -141,14 +143,15 @@ public class WaitingRoom extends Room {
 	 * 이 함수는 사용자가 PIN 번호를 입력하고 방에 들었가기를 눌렀을 때 사용될 함수이다. 사용자가 올바른 PIN 번호를 입력했을 때만 방으로
 	 * 연결해 준다.
 	 * 
-	 * @param PIN: 들어가고 싶은 채팅방의 핀번호
+	 * @param PIN:
+	 *            들어가고 싶은 채팅방의 핀번호
 	 * @return 오류 없이 방에 들어갔으면 true를 리턴해준다.
 	 */
 	private static boolean enterChatRoom(int PIN) {
 		if (chatRoomServerSockets.containsKey(PIN)) {
 			try {
-				new ChatRoom(chatRoomServerSockets.get().accept(),PIN,"user1","user@naver.com").start();
-				new ChatRoom(chatRoomServerSockets.get)
+				new ChatRoom(chatRoomServerSockets.get(PIN).accept()).start();
+
 				System.out.println("Method enterChatroom successed");
 				return true;
 			} catch (Exception e) {
@@ -157,62 +160,64 @@ public class WaitingRoom extends Room {
 		}
 		return false;
 	}
+	// End ChatRoom
+	// Method=======================================================================================
 
+	// 파일룸
+	/**
+	 * makeFileRoom
+	 * 
+	 * 이 함수는 사용자가 방 만들기 버튼을 누르고, 올바른 옵션을 입력한 후 채팅방의 서버소켓을 할당하는 함수 이다. 유니크한 핀번호를 가지게
+	 * 될때 까지 핀 번호를 할당을 시도한다.
+	 * 
+	 * @return : 파일룸의 PIN 번호를 리턴해준다. (채팅방의 PIN 번호는 짝수이다.)
+	 */
+	private int makeFileRoom() throws Exception {
+		int PIN;
+		System.out.println("Enter makeFileRoom");
+		synchronized (fileRoomServerSockets) {
 
-	   // 파일룸
-	   /**
-	    * makeFileRoom
-	    * 
-	    * 이 함수는 사용자가 방 만들기 버튼을 누르고, 올바른 옵션을 입력한 후 채팅방의 서버소켓을 할당하는 함수 이다. 유니크한 핀번호를 가지게
-	    * 될때 까지 핀 번호를 할당을 시도한다.
-	    * 
-	    * @return : 파일룸의 PIN 번호를 리턴해준다. (채팅방의 PIN 번호는 짝수이다.)
-	    */
-	   private int makeFileRoom() throws Exception {
-	      int PIN;
-	      System.out.println("Enter makeFileRoom");
-	      synchronized (fileRoomServerSockets) {
+			PIN = makePIN() + 1;
+			if (fileRoomServerSockets.containsKey((Integer) PIN)) {
+				System.out.println("eeeeeeaaaaak - file");
+			}
+			ServerSocket tempSV = new ServerSocket(PIN);
 
-	         PIN = makePIN()+1;
-	         if (fileRoomServerSockets.containsKey((Integer) PIN)) {
-	            System.out.println("eeeeeeaaaaak - file");
-	         }
-	         ServerSocket tempSV = new ServerSocket(PIN);
+			fileRoomServerSockets.put(PIN, tempSV);
+		}
+		System.out.println("end makeFileRoom");
+		return PIN;
+	}
 
-	         fileRoomServerSockets.put(PIN, tempSV);
-	      }
-	      System.out.println("end makeFileRoom");
-	      return PIN;
-	   }
+	/**
+	 * enterFileRoom
+	 * 
+	 * 이 함수는 사용자가 PIN 번호를 입력하고 방에 들었가기를 눌렀을 때 사용될 함수이다. 사용자가 올바른 PIN 번호를 입력했을 때만 방으로
+	 * 연결해 준다.
+	 * 
+	 * @param PIN
+	 *            - 들어가고 싶은 채팅방의 핀번호
+	 * @return 오류 없이 방에 들어갔으면 true를 리턴해준다.
+	 */
+	private static boolean enterFileRoom(int PIN) {
+		if (fileRoomServerSockets.containsKey(PIN)) {
+			try {
+				new ChatRoom(fileRoomServerSockets.get(PIN).accept()).start();
+				System.out.println("enterFileroom very good");
+				return true;
+			} catch (Exception e) {
+				System.out.println("Error in enterFileroom");
+			}
+		}
+		System.out.println("FFFFFFFFFF - file");
+		return false;
+	}
 
-	   /**
-	    * enterFileRoom
-	    * 
-	    * 이 함수는 사용자가 PIN 번호를 입력하고 방에 들었가기를 눌렀을 때 사용될 함수이다. 사용자가 올바른 PIN 번호를 입력했을 때만 방으로
-	    * 연결해 준다.
-	    * 
-	    * @param PIN
-	    *            - 들어가고 싶은 채팅방의 핀번호
-	    * @return 오류 없이 방에 들어갔으면 true를 리턴해준다.
-	    */
-	   private static boolean enterFileRoom(int PIN) {
-	      if (fileRoomServerSockets.containsKey(PIN)) {
-	         try {
-	            new ChatRoom(fileRoomServerSockets.get(PIN).accept()).start();
-	            System.out.println("enterFileroom very good");
-	            return true;
-	         } catch (Exception e) {
-	            System.out.println("Error in enterFileroom");
-	         }
-	      }
-	      System.out.println("FFFFFFFFFF - file");
-	      return false;
-	   }
-	   
 	/**
 	 * makePIN
 	 * 
 	 * 올바른 핀번호를 램던으로 할당해 준다.
+	 * 
 	 * @return 10이상에서 100000 미만의 숫자 중 할당되지 않은 번호를 리턴한다.
 	 */
 	private static int makePIN() {
