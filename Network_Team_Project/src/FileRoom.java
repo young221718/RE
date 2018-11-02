aebf556e7ebd4a35b938042476fc4910c30def5c
YoonHyeJu <sg51230@naver.com>
2018-11-02 14:30:00 +0900

    file

--- Network_Team_Project/src/FileRoom.java
+++ Network_Team_Project/src/FileRoom.java
@@ -1,4 +1,55 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.io.File;
+import java.io.FileOutputStream;
 
-public class FileRoom {
+import basic.Room;
 
+public class FileRoom extends Room{
+
+   public FileRoom(Socket socket) {
+      super(socket);
+   }
+   public FileRoom(Socket socket, int port, String group_name)
+   {
+      super(socket);
+      portNumber = port; 
+      groupName = group_name;   
+   }
+   
+   public void run() {
+      System.out.println("Enter the FileRoom!");
+      try {
+         fromClient = new ObjectInputStream(roomSocket.getInputStream());
+         toClient = new ObjectOutputStream(roomSocket.getOutputStream());
+         
+         groupName = (String)fromClient.readObject();
+         File f = new File("C:\\Users\\", groupName);
+         FileOutputStream output = new FileOutputStream(f);
+         
+         byte[] buffer = new byte[1024];
+         int bytesRead = 0;
+         while ((bytesRead = fromClient.read(buffer))>=0) {
+            toClient.write(buffer, 0, bytesRead);
+         }
+         toClient.flush();
+         System.out.println("file recieve success");
+         
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      finally {
+         try {
+            roomSocket.close();
+         } catch (IOException e) {
+            
+         }
+      }
+      
+      
+   }
 }
--- Network_Team_Project/src/basic/Room.java
+++ Network_Team_Project/src/basic/Room.java
@@ -5,18 +5,17 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public abstract class Room extends Thread{
-	protected ServerSocket welcomeSocket;
-	protected Socket roomSocket;
-	protected ObjectInputStream fromClient;
-	protected ObjectOutputStream toClient;
-	protected int portNumber;
-	protected int protocol;
-	protected String Email;
-	protected String Name;
-	
-	public Room(Socket socket){
-		this.roomSocket = socket;
-	}
-	
-	
+   protected ServerSocket welcomeSocket;
+   protected Socket roomSocket;
+   protected ObjectInputStream fromClient;
+   protected ObjectOutputStream toClient;
+   protected int portNumber;
+   protected int protocol;
+   protected String Email;
+   protected String Name;
+   protected String groupName;
+   
+   public Room(Socket socket){
+      this.roomSocket = socket;
+   }
 }
