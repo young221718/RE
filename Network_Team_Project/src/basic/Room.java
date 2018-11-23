b2940b3afe67b8bbd9d0cddc56c7709f0763a69f
Young2218 <44011517+Young2218@users.noreply.github.com>
2018-11-23 20:11:32 +0900

    Revert "Revert "데이터베이스 거의 완료""
    This reverts commit 8e23bc20a564330a21d0a01a3da831817ebf59d8.

--- Network_Team_Project/src/basic/Room.java
+++ Network_Team_Project/src/basic/Room.java
@@ -4,12 +4,15 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import server.Database;
+
 public abstract class Room extends Thread{
    protected ServerSocket welcomeSocket;
    protected Socket roomSocket;
    protected ObjectInputStream fromClient;
    protected ObjectOutputStream toClient;
    protected RoomInformation roomInfor;
+   protected Database db;
    protected int portNumber;
    protected int protocol;
    
