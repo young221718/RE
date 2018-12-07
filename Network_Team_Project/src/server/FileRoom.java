9ac5310bdf0704d3a818924bdf9ee818e5aede14
YoonHyeJu <sg51230@naver.com>
2018-12-07 21:55:52 +0900

    파일룸 주석

--- Network_Team_Project/src/server/FileRoom.java
+++ Network_Team_Project/src/server/FileRoom.java
@@ -24,38 +24,59 @@ public class FileRoom extends Room {
 		this.portNumber = port;
 	}
 
+	/*
+	 * <<<Explain The FileRoom>>
+	 * After client enter the file room, FileRoom receives the protocol from the client.
+	 * The client sends protocol '77' each time that it sends a file one by one.
+	 * Every time FileRoom receives the protocol '77', FileRoom reads and saves the file.
+	 * FileRoom reads the file during the protocol is 0.
+	 * When the sign goes to '99', it means that there are no more files to read.
+	 * The file that client send is stored in a folder created by the pin number of the room.
+	 * Each file is stored with the user's email and index to avoid duplication.
+	 * (non-Javadoc)
+	 * @see java.lang.Thread#run()
+	 */
 	public void run() {
-		int a = 0;
+		int a = 0; // For counting file number
 		System.out.println("Enter the FileRoom!");
 		try {
-		
+			// Used to receive data from customer.
 			BufferedInputStream up = new BufferedInputStream(roomSocket.getInputStream());
 			DataInputStream fromClient = new DataInputStream(up);
 
-			int sign = 0;
+			int sign = 0; // To send a sign to read the file.
 
-			String group_name = db.GetRoomName(portNumber);
-			
 			do {
-				int fPro = up.read(); // 프로토콜
+				int fPro = up.read(); // Read the protocol that the client sends a picture
 
-				if (fPro == 77) {
-					int len = fromClient.readInt();
-					System.out.println(len + " received");
-					File f = new File("C:\\RE\\" + portNumber);
+				if (fPro == 77) { // Client sends file
+					int len = fromClient.readInt(); // file's length
+					System.out.println(len + " received"); 
+					
+					// Create folder to save files according to group's port number
+					File f = new File("C:\\RE\\" + portNumber); 
+					
+					// Create folder that does not exist.
 					if (f.exists() == false)
-						f.mkdirs();// folderNN - 폴더, groupNB - group_id
+						f.mkdirs();
 
+					// Save each file without duplication by user's email and index 
 					FileOutputStream toFile = new FileOutputStream(f + "\\" + email + a + ".png");
+					
+					//Write the file using outFile
 					BufferedOutputStream outFile = new BufferedOutputStream(toFile);
-					int ch = 0;
-
+				
+					// It takes the for loop as much as the file length, receives it from the client, and stores it in the file.
 					for (int i = 0; i < len; i++) {
 						outFile.write(up.read());
 					}
+					
+					// Read sign to check if any files are left
 					sign = fromClient.read();
-					a++;
-					outFile.flush();
+					
+					a++; // file index
+					
+					outFile.flush(); //If there is anything left in the buffer
 					outFile.close();
 				}
 			} while (sign != 99);
